aefe55cc5c0f07a0e422357689064e59af3d419c
MikeStegall <mestegall5@gmail.com>
2017-09-26 22:07:35 -0500

    updated the README.md

--- README.md
+++ README.md
@@ -1,8 +1,76 @@
 # Scheduled Maintenance App
 
-This project was bootstrapped with [Create React App], [Ratchet], and [Mori]
+This project is my final project at [The Iron Yard] after taking the 24 week course. This project shows what i am able to do with Functional React with an immutable data structure.
+
+### Tools Used
+* [Create React App]
+* [Ratchet]
+* [Mori]
+* [Firebase]
+
+### Installation
+
+```
+
+git clone git@github.com:MikeStegall/scheduled-maintenance.git
+cd scheduled-maintenance
+npm install or yarn add
+npm start or yarn start
+open http://localhost:3000/
+
+```
+
+### Things That You Will Need to Add
+
+Need to go to [Firebase] and create a data base.
+
+Create a `firebase.js` file inside of the src folder and paste
+
+```
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+    apiKey: "YOUR API KEY",
+    authDomain: "app-name.firebaseapp.com",
+    databaseURL: "https://app-name.firebaseio.com",
+    projectId: "app-name",
+    storageBucket: "app-name.appspot.com",
+    messagingSenderId: ""
+  }
+firebase.initializeApp(config)
+
+export default firebase
+```
+
+### The Usefulness of this App
+
+This app is for computer technicians that go out to client sites and perform an 11 point check list on their computers. You can use it with a phone or tablet to enter in all the data that you acquire.
+
+The 11 point check list includes:
+* Number of Virus Software Updates
+* Free Space on Disk
+* Size of Temporary Files
+* Fragmentation of Hard Drive
+* Whether or not you can blow out the PC
+* Number of Windows Updates
+* If Viruses/Malware found
+* If the Hard Drive is good
+* If the Event Logs have critical Errors
+* If the System Files were corrupt or not
+* Whether the computers is a server and if the server is backing up and working.
+
+After the technician fills out this information through the number of computers that the company has it displays the average score to the company. When the technician comes back home they can pull up the company information they just entered.
+
+![Spreadsheet Screen Shot](spreadsheetScreenShot.png)
+
+### Different applications
+
+This app as several different uses. If you have a check list that you want to have you can do it with this app. Whether is be Car Maintenance, cleaning a house or what ever your mind can thing of. The possibilities are endless.
 
 
 [Create React App]: https://github.com/facebookincubator/create-react-app
 [Ratchet]: http://goratchet.com/
-[Mori]: serverBackups
+[Mori]: http://swannodette.github.io/mori/
+[Firebase]: https://firebase.google.com/
+[The Iron Yard]: https://www.theironyard.com/
--- src/Layout/ErrorMessage.js
+++ src/Layout/ErrorMessage.js
@@ -2,6 +2,27 @@ import React from 'react'
 import mori from 'mori'
 import MoriComponent from '../MoriComponent'
 
+function forPage1 (idx) {
+  const newState = mori.assocIn(window.CURRENT_STATE, ['computers', idx, 'computerInputStep'], 1)
+  window.NEXT_STATE = newState
+}
+function forPage2 (idx) {
+  const newState = mori.assocIn(window.CURRENT_STATE, ['computers', idx, 'computerInputStep'], 2)
+  window.NEXT_STATE = newState
+}
+function forPage3 (idx) {
+  const newState = mori.assocIn(window.CURRENT_STATE, ['computers', idx, 'computerInputStep'], 3)
+  window.NEXT_STATE = newState
+}
+function forPage4 (idx) {
+  const newState = mori.assocIn(window.CURRENT_STATE, ['computers', idx, 'computerInputStep'], 4)
+  window.NEXT_STATE = newState
+}
+function forPage5 (idx) {
+  const newState = mori.assocIn(window.CURRENT_STATE, ['computers', idx, 'computerInputStep'], 5)
+  window.NEXT_STATE = newState
+}
+
 function ComputerName (idx) {
   const computerName = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'computerName'])
   if (computerName === ('Computer ' + (idx + 1))) {
@@ -10,86 +31,96 @@ function ComputerName (idx) {
 }
 
 function NullVirusUpdates (idx) {
+  let clickForPage1 = forPage1.bind(null, idx)
   const checkForVirusUpdates = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'checkForVirusUpdates'])
   if (checkForVirusUpdates === null) {
-    return <div>Go Back to page 1 and check the Virus Software Updates</div>
+    return <div onClick={clickForPage1}>Click here and check the Virus Software Updates</div>
   }
 }
 
 function NullFreeDiskSpace (idx) {
+  let clickForPage1 = forPage1.bind(null, idx)
   const freeDiskSpace = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'freeDiskSpace'])
   if (freeDiskSpace === null) {
-    return <div>Go Back to page 1 and check Free Disk Space</div>
+    return <div onclick={clickForPage1}>Click here and check Free Disk Space</div>
   }
 }
 
 function NullFileSize (idx) {
+  let clickForPage2 = forPage2.bind(null, idx)
   const sizeOfTempFiles = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'sizeOfTempFiles'])
   if (sizeOfTempFiles === null) {
-    return <div>Go Back to page 2 and check size of temp files.</div>
+    return <div onClick={clickForPage2}>Click here and check size of temp files.</div>
   }
 }
 
 function NullFragmentation (idx) {
+  let clickForPage2 = forPage2.bind(null, idx)
   const fragmentation = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'fragmentation'])
   if (fragmentation === null) {
-    return <div>Go Back to page 2 and check Fragmentations</div>
+    return <div onClick={clickForPage2}>Click here and check Fragmentations</div>
   }
 }
 
 function PcCleanedNotes (idx) {
+  let clickForPage3 = forPage3.bind(null, idx)
   const hasPcBeenCleaned = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'hasPcBeenCleaned'])
   const pcCleanedNotes = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'pcCleanedNotes'])
   if (!hasPcBeenCleaned) {
     if (pcCleanedNotes === '') {
-      return <div>Please enter Notes on PClean on Page 3</div>
+      return <div onClick={clickForPage3}>Click here and enter notes on PClean</div>
     }
   }
 }
 
 function NullWindowsUpdates (idx) {
+  let clickForPage3 = forPage3.bind(null, idx)
   const numberOfWindowsUpdates = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'numberOfWindowsUpdates'])
   if (numberOfWindowsUpdates === null) {
-    return <div>Go Back to Page 3 and check Number of Windows Updates</div>
+    return <div onClick={clickForPage3}>Click here and check number of Windows Updates</div>
   }
 }
 
 function VirusesFoundNotes (idx) {
+  let clickForPage4 = forPage4.bind(null, idx)
   const hasVirusBeenFound = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'hasVirusBeenFound'])
   const virusesFoundNotes = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'virusesFoundNotes'])
   if (hasVirusBeenFound) {
     if (virusesFoundNotes === '') {
-      return <div>Please enter Notes about Virus Found on Page 4</div>
+      return <div onClick={clickForPage4}>Click here and enter notes about Virus Found</div>
     }
   }
 }
 
 function HardDriveHealthNotes (idx) {
+  let clickForPage4 = forPage4.bind(null, idx)
   const isHardDriveGood = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'isHardDriveGood'])
   const hardDriveHealthNotes = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'hardDriveHealthNotes'])
   if (!isHardDriveGood) {
     if (hardDriveHealthNotes === '') {
-      return <div>Please enter Notes about the hard drives health on Page 4</div>
+      return <div onClick={clickForPage4}>Click here and enter notes about the hard drives health</div>
     }
   }
 }
 
 function EventLogsNotes (idx) {
+  let clickForPage5 = forPage5.bind(null, idx)
   const hasCritcalEventLogs = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'hasCritcalEventLogs'])
   const eventLogsNotes = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'eventLogsNotes'])
   if (hasCritcalEventLogs) {
     if (eventLogsNotes === '') {
-      return <div>Please enter Notes about Event Logs on Page 5</div>
+      return <div onClick={clickForPage5}>Click here and enter Notes about Event Logs</div>
     }
   }
 }
 
 function SystemFileCheckNotes (idx) {
+  let clickForPage5 = forPage5.bind(null, idx)
   const hasCurroptedSystemFiles = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'hasCurroptedSystemFiles'])
   const systemFileCheckNotes = mori.getIn(window.CURRENT_STATE, ['computers', idx, 'systemFileCheckNotes'])
   if (hasCurroptedSystemFiles) {
     if (systemFileCheckNotes === '') {
-      return <div>Please enter Notes about Curropted files on Page 5</div>
+      return <div onClick={clickForPage5}>Click here and enter Notes about Curropted files</div>
     }
   }
 }
