ff3074d196908e75da7cbadee188b56f462c9c78
MikeStegall <mestegall5@gmail.com>
2017-10-10 17:03:58 -0500

    Fixed bug where the site would crash after finishing a job and trying to continue an incomplete job

--- src/Layout/NewJobButton.js
+++ src/Layout/NewJobButton.js
@@ -10,11 +10,12 @@ function clickNewJobButton () { // basically resets the state
   const newState6 = mori.assoc(newState5, 'companyName', '')
   const newState7 = mori.assoc(newState6, 'numComputers', '')
   const newState8 = mori.assoc(newState7, 'activeComputerIdx', 0)
-  const newState9 = mori.assoc(newState8, 'companyAverage', 0)
+  const newState9 = mori.assoc(newState8, 'companyAverage', null)
   const newState10 = mori.assoc(newState9, 'step', 1)
-  const newState11 = mori.assoc(newState10, 'time', new Date())
+  const newState11 = mori.assoc(newState10, 'companyId', '')
   const newState12 = mori.assoc(newState11, 'showIncompleteJobs', false)
-  window.NEXT_STATE = newState12
+  const newState13 = mori.assoc(newState12, 'incompleteJobArr', [])
+  window.NEXT_STATE = newState13
 }
 
 function NewJobButton () {
