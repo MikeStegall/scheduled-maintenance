42af4b66f81e8cd724a69fbda63c613fa12e76ab
MikeStegall <mestegall5@gmail.com>
2017-10-10 16:18:01 -0500

    fixed a bug where companies wouldnt be finished

--- src/Layout/CompanyAverage.js
+++ src/Layout/CompanyAverage.js
@@ -16,11 +16,12 @@ function ShowCompanyAverage (idx, numComputers) {
   const companyAverageArrSum = mori.reduce(mori.sum, 0, companyAverageArr)
   const companyAverage = companyAverageArrSum / companyAverageArrLenth
   const companyAverageDecimal = companyAverage.toFixed(2) // displays the average to 2 decimal points
-  if (companyAverageDecimal !== null) {
-    window.NEXT_STATE = mori.assoc(window.CURRENT_STATE, 'allComputersFinished', true)
-  }
-  const newState = mori.assoc(window.CURRENT_STATE, 'companyAverage', companyAverageDecimal)
-  window.NEXT_STATE = newState
+  // if (companyAverageDecimal >= 0) {
+  //   window.NEXT_STATE = mori.assoc(window.CURRENT_STATE, 'allComputersFinished', true)
+  // }
+  const newState1 = mori.assoc(window.CURRENT_STATE, 'companyAverage', companyAverageDecimal)
+  const newState2 = mori.assoc(newState1, 'allComputersFinished', true)
+  window.NEXT_STATE = newState2
   const companyAverageJs = mori.toJs(companyAverageDecimal) // converts the average from closureScript to javaScript so it can be displayed to the page
   return <div>{companyAverageJs}</div>
 }
@@ -33,6 +34,9 @@ class CompanyAverage extends MoriComponent {
     const companyNameJs = mori.toJs(companyName)
     const companyAverage = mori.get(window.CURRENT_STATE, 'companyAverage')
     const companyAverageJs = mori.toJs(companyAverage)
+    if (companyAverageJs >= 0) {
+      window.NEXT_STATE = mori.assoc(window.CURRENT_STATE, 'allComputersFinished', true)
+    }
 
     pushFireBase()
 
